baec6d6b1d0b54f49fa7cfa13e297ba83dc0d126
Girija kangutkar <51635470+girijakangutkar@users.noreply.github.com>
2022-01-24 13:39:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["red","green","yellow","cyan","purple","pink","grey","silver","blue","brown","lightgreen","skyblue"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
